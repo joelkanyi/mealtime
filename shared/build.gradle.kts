0b4d6398a33f9407ffceb2576393e8b5fa55b834
JoelKanyi <joelkanyi98@gmail.com>
2023-06-04 17:32:13 +0300

    create shared module

---
+++ shared/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+}
+
+kotlin {
+    android {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                //put your multiplatform dependencies here
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.joelkanyi.shared"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 24
+    }
+}
